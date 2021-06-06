fe590e39770e3be0172860f832094405e77f408b
Michelle <michellehnguyen03@gmail.com>
2021-06-06 18:44:37 -0400

    Added version 2 of password generator
    This one includes symbols and numbers. It also asks for user input for length

--- main.py
+++ main.py
@@ -1,28 +1,66 @@
 # Importing the relevant modules
 from random import randint
-
+from random import sample
 
 def main():
-    password = upper_case()
-    print(f"Here is your password: {password}")
+    password_choice = input("Type 1 if you want uppercase only\n"
+                            "Type 2 if you want both uppercase and lowercase\n"
+                            "Type 3 if you want more symbols\n")
+    length = int(input("Choose the length of your password (number only): "))
+    if password_choice == "1":
+        password = upper_case(length)
+        print(f"\nHere is your password: {password}")
+    elif password_choice == "2":
+        password = upper_and_lowercase(length)
+        print(f"\nHere is your password: {password}")
+    elif password_choice == "3":
+        password = more_symbols(length)
+        print(f"\nHere is your password: {password}")
+    else:
+        print("That is not a valid input. ")
+
+    # password = more_symbols(length)
+    # password = upper_case(length)
+    # password = upper_and_lowercase(length)
 
 
-def upper_case():
+
+def upper_case(length):
     # All uppercase password
     password = ""
-    for i in range(10):
+    for i in range(length):
         i = chr(randint(65, 90))
         password = str(password) + i
     return password
 
-
-def upper_and_lowercase():
+def upper_and_lowercase(length):
     # Upper and lowercase password
     password = ""
-    for i in range(5):
+    for i in range(int(length / 2)):
         i = chr(randint(65, 90))
         j = chr(randint(65, 90)).lower()
         password = str(password) + i + j
+    return password
+
+def more_symbols(length):
+    uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_letters = uppercase_letters.lower()
+    digits = "0123456789"
+    symbols = "!@#$%^&*(){}'/;:"
+    upper, lower, nums, syms = True, True, True, True
+
+    all = ""
+    if upper:
+        all += uppercase_letters
+    if lower:
+        all += lowercase_letters
+    if nums:
+        all += digits
+    if syms:
+        all += symbols
+
+
+    password = "".join(sample(all, length))
     return password
 
 
