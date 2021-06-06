93ca0f1f927b0075d407185699d9983ac6998d72
Michelle <michellehnguyen03@gmail.com>
2021-06-06 17:53:10 -0400

    Version 1

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+# Importing the relevant modules
+from random import randint
+
+
 def main():
-    print('test')
+    password = upper_case()
+    print(f"Here is your password: {password}")
+
+
+def upper_case():
+    # All uppercase password
+    password = ""
+    for i in range(10):
+        i = chr(randint(65, 90))
+        password = str(password) + i
+    return password
+
+
+def upper_and_lowercase():
+    # Upper and lowercase password
+    password = ""
+    for i in range(5):
+        i = chr(randint(65, 90))
+        j = chr(randint(65, 90)).lower()
+        password = str(password) + i + j
+    return password
+
 
 if __name__ == "__main__":
     main()
-        
